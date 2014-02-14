5182c434446aeac5bb11785849bd4bb01ae1fb6f
Marc Thomas <mathomastech@gmail.com>
2014-02-14 14:05:05 -0600

    Fixed false input of "" and null entries

--- ui.py
+++ ui.py
@@ -52,18 +52,14 @@ class Handler:
 
     def on_submitButton_clicked(self, *args):
         info_buffer = info_display.get_buffer()
-        chat_input = chat_entry.get_text()        
-        chat_input += '\n' 
-        info_buffer.insert(info_buffer.get_end_iter(),chat_input)
-        chat_entry.set_text("")
+        chat_input = chat_entry.get_text()
+        if (chat_input != ""):        
+            chat_input += '\n' 
+            info_buffer.insert(info_buffer.get_end_iter(),chat_input)
+            chat_entry.set_text("")
 
     def on_chatEntry_activate(self, *args):
         Handler.on_submitButton_clicked(self, *args)
-        #info_buffer = info_display.get_buffer()
-        #chat_input = chat_entry.get_text()        
-        #chat_input += '\n' 
-        #info_buffer.insert(info_buffer.get_end_iter(),chat_input)
-        #chat_entry.set_text("")
 
 gui.connect_signals(Handler())
 app.show_all()
