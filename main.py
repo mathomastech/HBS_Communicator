cb5ebf612da10578d9881beb8814e6084e0fec89
Marc Thomas <mathomastech@gmail.com>
2014-02-18 12:19:02 -0600

    Refactoring

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-from gi.repository import Gtk
-from ui import *
-import os.path
-
-
-#Global Variables
-f = open("config.txt", 'r')
-configs = f.readline()
-f.close()
-config = configs.split(",")
-
-LOG_PATH = config[0]
-ACTIVE_LOG_PATH = config[1]
-ROSTER_PATH = config[2]
-ACTIVE_ROSTER_PATH = config[3]
-USER_PATH = config[4]
-USER = config[5]
-
-
+from configuration import *
+from communicator import *
+from handler import *
 
 def main():
     print("success")
