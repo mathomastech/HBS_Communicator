6af64ea73e97bb0601cf46635c2ce501183d7448
Marc Thomas <mathomastech@gmail.com>
2014-03-14 10:38:50 -0500

    Configuration File

---
+++ config.py
@@ -0,0 +1,13 @@
+
+#Modify this file to configure to your vbulletin database
+
+# This is a python dictionary. The first entry's (ie. 'Logs', 'Roster' etc) should remain the same.
+# The second entry's ('PATH_TO_LOGS', 'PATH_TO_ROSTERS' should be reconfigured for your use.
+
+class Configuration():
+    c = {'Logs' : 'PATH_TO_LOGS', 
+        'Rosters'  : 'PATH_TO_ROSTERS', 
+        'DB IP' : '123.456.78.91',
+        'DB Name' : 'DATABASE_NAME',
+        'DB User' : 'DATABASE_USER', 
+        'DB Password' : 'DATABASE_PASSWORD'}
