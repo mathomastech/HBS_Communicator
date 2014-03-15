d4376f78cc317b5779d2bbd939adcc1d9fd20096
Marc Thomas <mathomastech@gmail.com>
2014-03-15 17:51:08 -0500

    Added connections to ssh server.

--- communicator.py
+++ communicator.py
@@ -1,45 +1,36 @@
 from configuration import *
 from database import *
+from ssh import *
 
 import os.path
 
 #populate TextView with Welcome Message
-f = open(LOG_PATH + "welcomeMessage.txt", 'r')
-info_buffer = info_display.get_buffer()
-chat_input = f.read()
-info_buffer.set_text(chat_input) 
-f.close()
+#f = open(LOG_PATH + "welcomeMessage.txt", 'r')
+#info_buffer = info_display.get_buffer()
+#chat_input = f.read()
+#info_buffer.set_text(chat_input) 
+#f.close()
 
 class Communicator:
     USER = ""
     USER_PERMISSIONS = []
     ACTIVE_LOG_PATH = ""
     ACTIVE_ROSTER_PATH = ""
+    SSH_CONNECTION = SSH.connect_to_ssh()
 
     def channel_switch(log_path,roster_path):
-        if os.path.isfile(log_path):
-            Communicator.write_to_channel(log_path)
-        else:
-            Communicator.write_to_channel_no_log(log_path)
+        log = SSH.get_log(Communicator.SSH_CONNECTION, log_path)
+        Communicator.write_to_channel(log_path,log)
         if os.path.isfile(roster_path):
             Communicator.write_to_roster(roster_path)
         else:
             Communicator.write_to_roster_no_roster(roster_path)
 
-    def write_to_channel(current_log_path):
-        f = open(current_log_path, 'r')
+    def write_to_channel(current_log_path,log):
         info_buffer = info_display.get_buffer()
-        chat_input = f.read()
-        info_buffer.set_text(chat_input) 
-        f.close()
+        info_buffer.set_text(log) 
         Communicator.ACTIVE_LOG_PATH = current_log_path
 
-    def write_to_channel_no_log(current_log_path):
-        info_buffer = info_display.get_buffer()
-        chat_input = "No logs for this channel yet.\n"
-        info_buffer.set_text(chat_input) 
-        Communicator.ACTIVE_LOG_PATH = current_log_path
-        
     def write_to_roster(current_roster_path):
         f = open(current_roster_path, 'r')
         info_buffer = roster_display.get_buffer()
