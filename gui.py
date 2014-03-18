8fa2979735897383b35c3d5b77346bcf4317e51e
Marc Thomas <mathomastech@gmail.com>
2014-03-18 16:09:26 -0500

    Refactored configuration.py into gui.py

---
+++ gui.py
@@ -0,0 +1,53 @@
+from gi.repository import Gtk
+
+class GUI():
+    #Initialize communicator XML objects
+    APP = Gtk.Builder()
+    APP.add_from_file("glade/communicator.glade")
+    MAIN_APP = APP.get_object("hbsCommunicator")
+
+    #Initialize LOGIN_WINDOW XML objects
+    LOGIN_WINDOW = Gtk.Builder()
+    LOGIN_WINDOW.add_from_file("glade/loginWindow.glade")
+    AUTHORIZE_WINDOW = LOGIN_WINDOW.get_object("loginWindow")
+
+    #Command Buttons
+    CENTRAL_CMD_BTN = APP.get_object("centralCommandButton")
+    OPERATIONS_CMD_BTN = APP.get_object("operationsCommandButton")
+    COD_CMD_BTN = APP.get_object("codCommandButton")
+    RUST_CMD_BTN = APP.get_object("rustCommandButton")
+    GW_CMD_BTN = APP.get_object("gwCommandButton")
+    WOW_CMD_BTN = APP.get_object("wowCommandButton")
+    MC_CMD_BTN = APP.get_object("mcCommandButton")
+    DAYZ_CMD_BTN = APP.get_object("armaCommandButton")
+    LOGISTICS_CMD_BTN = APP.get_object("logisticsCommandButton")
+    MP_CMD_BTN = APP.get_object("mpCommandButton")
+    ADMISSIONS_CMD_BTN = APP.get_object("admissionsCommandButton")
+
+    #General Buttons
+    GENERAL_BTN = APP.get_object("generalButton")
+    COD_BTN = APP.get_object("ccodGeneralButton")
+    RUST_BTN = APP.get_object("rustGeneralButton")
+    GW_BTN = APP.get_object("gwGeneralButton")
+    WOW_BTN = APP.get_object("wowGeneralButton")
+    MC_BTN = APP.get_object("mcGeneralButton")
+    DAYZ_BTN = APP.get_object("armaGeneralButton")
+    
+    #Channel and Roster Elements
+    CHAT_ENTRY = APP.get_object("chatEntry")
+    SUBMIT_BTN = APP.get_object("submitButton")
+    INFO_DISPLAY = APP.get_object("infoDisplay")
+    ROSTER_DISPLAY = APP.get_object("rosterDisplay")
+    
+    #Login Window Text Entrys
+    LOGIN_BTN = APP.get_object("loginWindowButton")
+    USERNAME_ENTRY = LOGIN_WINDOW.get_object("usernameEntry")
+    PASSWORD_ENTRY = LOGIN_WINDOW.get_object("passwordEntry")
+    LOGIN_STATUS_LABEL = LOGIN_WINDOW.get_object("loginStatusLabel")
+
+    #LOG_PATH = Configuration.c['Logs']
+    #ROSTER_PATH = Configuration.c['Rosters']
+    #DB_IP = Configuration.c['DB IP']
+    #DB_NAME = Configuration.c['DB Name']
+    #DB_USER = Configuration.c['DB User']
+    #DB_PASSWORD = Configuration.c['DB Password']
