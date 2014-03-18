d5030ecbe9232d952776bd33ce89a4639ac7ec59
Marc Thomas <mathomastech@gmail.com>
2014-03-18 16:09:57 -0500

    Renamed class from Configuration to config

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
-
 #Modify this file to configure to your vbulletin database
 
 # This is a python dictionary. The first entry's (ie. 'Logs', 'Roster' etc) should remain the same.
 # The second entry's ('PATH_TO_LOGS', 'PATH_TO_ROSTERS' should be reconfigured for your use.
 
-class Configuration():
-    c = {'Logs' : 'PATH_TO_LOGS', 
-        'Rosters'  : 'PATH_TO_ROSTERS', 
-        'DB IP' : '123.456.78.91',
-        'DB Name' : 'DATABASE_NAME',
-        'DB User' : 'DATABASE_USER', 
-        'DB Password' : 'DATABASE_PASSWORD'}
+
+
+class Config():
+    c = {'Logs' : 'logs/', 
+        'Rosters'  : 'roster/', 
+        'DB IP' : '192.185.45.86',
+        'DB Name' : 'gbgraphi_vbulletin',
+        'DB User' : 'gbgraphi_centcom', 
+        'DB Password' : '@455g+)F6P0D',
+        'SSH IP' : '68.117.47.93',
+        'SSH User' : 'hbs',
+        'SSH Password' : 'hbsR0kz'
+}
