ca0ca6d4c395c4d78ed98687a43dfa026bd75bd8
Marc Thomas <mathomastech@gmail.com>
2014-03-26 21:44:12 -0500

    Added several new widget references.

--- gui.py
+++ gui.py
@@ -3,9 +3,16 @@ class GUI():
     CHANNEL_DISPLAY = ""
     ROSTER_DISPLAY = ""
     CHAT_ENTRY = ""
+    
+    # Login Window Widgets
+    LOGIN_GROUP_BOX = ""
+    
     LOGIN_STATUS_LABEL = ""
     LOGIN_BTN = ""    
-
+    USERNAME_LABEL = ""
+    USERNAME_ENTRY = ""
+    PASSWORD_LABEL = ""
+    PASSWORD_ENTRY = ""
 
 '''
     #Initialize communicator XML objects
