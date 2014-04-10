2858a41149060866c246503e9ca450591b32c642
Marc Thomas <mathomastech@gmail.com>
2014-04-10 17:00:01 -0500

    Worker for the multithreading logic

---
+++ worker.py
@@ -0,0 +1,25 @@
+import time
+from PyQt4 import QtCore
+from communicator import Communicator
+
+class Worker(QtCore.QThread):
+    def __init__(self, parent=None):
+        QtCore.QThread.__init__(self)
+
+    def refresh(self):
+        # Send signal to refresh the chat logs.
+        print("Refresh")
+        QtCore.QThread.emit(self,QtCore.SIGNAL('refresh'))
+
+    def run (self):
+        while True:
+            if(Communicator.ACTIVE_LOG_PATH != "" and
+                Communicator.ACTIVE_LOG_PATH != "logs/welcomeMessage.txt"):
+                print(Communicator.ACTIVE_LOG_PATH)
+                self.refresh()
+            time.sleep(1)
+        return
+
+    #def __del__(self):
+    #    self.wait()
+
