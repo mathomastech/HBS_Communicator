01ed4ff286da4446b3eebacbfdd962540702f7f3
Marc Thomas <mathomastech@gmail.com>
2014-04-14 23:59:12 -0500

    Adding infrastructure for a better refresh system.

--- ssh.py
+++ ssh.py
@@ -22,14 +22,21 @@ class SSH():
         return log
 
 
-    def get_logs(ssh,log_path):
+    def get_logs(ssh, LOCAL_LOGS, REMOTE_LOGS):
         # Get remote logs for all channels and return to communicator.
-        query = 'cat ' + log_path
-        stdin, stdout, stderr = ssh.exec_command(query)
-        log = stdout.readlines()
-        log = ''.join(log)
-
-        return log
+        keys = [k for k,v in Config.LOG_PATHS.items()]
+        for i in range (0, len(keys)):
+            for j in range (0, len(keys)):
+                if(keys[i] == REMOTE_LOGS[j][0]):
+                    query = 'cat ' + Config.LOG_PATHS[keys[i]]
+                    stdin, stdout, stderr = ssh.exec_command(query)
+                    log = stdout.readlines()
+                    log = ''.join(log)
+                    REMOTE_LOGS[j][1] = log
+                    #break
+                    
+        print(REMOTE_LOGS)
+        return LOCAL_LOGS, REMOTE_LOGS
 
 
     def write_to_log(ssh,log_path,log):
