47449a82375bfc44d9a28a17c9f444043b7a118c
Marc Thomas <mathomastech@gmail.com>
2014-04-21 09:48:43 -0500

    Reordering code alphebetically

--- communicator.py
+++ communicator.py
@@ -24,7 +24,27 @@ class Communicator:
 
     def __init__(self):
         super(Communicator,self).__init__()
-   
+    
+    def check_user_permissions(channel):
+        for i in range(0,len(GUI.CHANNELS)):
+            if channel == GUI.CHANNELS[i][GUI.CHANNEL_NAME]:
+                for j in range(len(Communicator.USER_PERMISSIONS)):
+                    for k in range(len(GUI.CHANNELS[i][GUI.PERMISSIONS])):
+                        if Communicator.USER_PERMISSIONS[j] == GUI.CHANNELS[i][GUI.PERMISSIONS][k]:
+                            return True
+        return False
+    
+    def enable_widgets():
+        GUI.CHAT_ENTRY.setEnabled(True)
+        GUI.SUBMIT_BTN.setEnabled(True)
+        GUI.CHANNEL_NOTEBOOK.setEnabled(True)
+        GUI.CONTENT_NOTEBOOK.setTabEnabled(1, True)
+        GUI.USER_BTN.setEnabled(True)
+    
+    def invalid_permissions():
+        # If user does not have appropriate permissions, print out message.
+        GUI.CHANNEL_DISPLAY.setPlainText("You do not have sufficient privileges to view this channel. If you feel this is in error, please contact an administrator")
+    
     def load_local_logs():
         for i in range(0,len(GUI.CHANNELS)):
             if(os.path.isfile(GUI.CHANNELS[i][GUI.SERVER_LOG_PATH])):
@@ -32,6 +52,39 @@ class Communicator:
                 log = f.read()
                 GUI.CHANNELS[i][GUI.STORED_LOG] = log
                 f.close()
+    
+    def login(username,password):
+        # Set user login and permissions
+        username = username.strip()
+        password = password.strip()
+        GUI.LOGIN_STATUS_LABEL.setText("Authenticating...")
+        flag, userPermissions, channelPermissions = Database.login(username,password)
+        #Communicator.populate_logs()
+        if flag:
+            # If True, set the username and user permissions. Hide the login and set 
+            # the welcome message and announcement. 
+            Communicator.USER = username
+            Communicator.USER_PERMISSIONS = userPermissions
+            Communicator.load_local_logs()
+            GUI.LOGIN_GROUP_BOX.resize(0,0)
+            Communicator.populate_channel(GUI.WELCOME_LOG, GUI.WELCOME_LOG )
+            Communicator.enable_widgets()
+            if Communicator.REMEMBER_LOGIN: 
+                f = open('credentials.txt', 'w')
+                username = username.strip()
+                f.write(username + "\n" + password) 
+                f.close()
+            elif os.path.exists('credentials.txt'):
+                os.remove('credentials.txt')
+
+    def populate_channel(log_path,roster_path):
+        # Populate the channel with both logs and rosters
+        log = SSH.get_active_log(Communicator.SSH_CONNECTION, log_path)
+        Communicator.write_to_channel(log_path,log)
+        if os.path.isfile(roster_path):
+            Communicator.write_to_roster(roster_path)
+        else:
+            Communicator.write_to_roster_no_roster(roster_path)
      
     def switch_command_channel(channel):
         if Communicator.check_user_permissions(channel):
@@ -68,15 +121,16 @@ class Communicator:
                 if(Communicator.DELTA[i] == GUI.CHANNELS[j][GUI.CHANNEL_NAME]):
                     GUI.CHANNELS[j][GUI.GUI_ELEMENT].setStyleSheet("color:red")
 
-    def populate_channel(log_path,roster_path):
-        # Populate the channel with both logs and rosters
-        log = SSH.get_active_log(Communicator.SSH_CONNECTION, log_path)
-        Communicator.write_to_channel(log_path,log)
-        if os.path.isfile(roster_path):
-            Communicator.write_to_roster(roster_path)
-        else:
-            Communicator.write_to_roster_no_roster(roster_path)
-
+    def write_chat_to_channel():
+        # Take chat box text and write it to channel and log.
+        chat_input = GUI.CHAT_ENTRY.text()
+        if (chat_input != ""):
+            log = Communicator.USER + ": " + chat_input
+            SSH.write_to_log(Communicator.SSH_CONNECTION,
+                                Communicator.ACTIVE_LOG_PATH, log)
+            Communicator.update_active_channel()
+            GUI.CHAT_ENTRY.setText("")
+    
     def write_to_channel(current_log_path,log):
         # Write log to the currently selected channel
         GUI.CHANNEL_DISPLAY.setPlainText(log)
@@ -95,75 +149,6 @@ class Communicator:
     def write_to_roster_no_roster(current_roster_path):
         GUI.ROSTER_DISPLAY.setPlainText("There is currently no roster for this channel\n")
         Communicator.ACTIVE_ROSTER_PATH = current_roster_path
-
-    def write_chat_to_channel():
-        # Take chat box text and write it to channel and log.
-        chat_input = GUI.CHAT_ENTRY.text()
-        if (chat_input != ""):
-            log = Communicator.USER + ": " + chat_input
-            SSH.write_to_log(Communicator.SSH_CONNECTION,
-                                Communicator.ACTIVE_LOG_PATH, log)
-            Communicator.update_active_channel()
-            GUI.CHAT_ENTRY.setText("")
-
-    def login(username,password):
-        # Set user login and permissions
-        username = username.strip()
-        password = password.strip()
-        GUI.LOGIN_STATUS_LABEL.setText("Authenticating...")
-        flag, userPermissions, channelPermissions = Database.login(username,password)
-        #Communicator.populate_logs()
-        if flag:
-            # If True, set the username and user permissions. Hide the login and set 
-            # the welcome message and announcement. 
-            Communicator.USER = username
-            Communicator.USER_PERMISSIONS = userPermissions
-            Communicator.load_local_logs()
-            GUI.LOGIN_GROUP_BOX.resize(0,0)
-            Communicator.populate_channel(GUI.WELCOME_LOG, GUI.WELCOME_LOG )
-            Communicator.enable_widgets()
-            if Communicator.REMEMBER_LOGIN: 
-                f = open('credentials.txt', 'w')
-                username = username.strip()
-                f.write(username + "\n" + password) 
-                f.close()
-            elif os.path.exists('credentials.txt'):
-                os.remove('credentials.txt')
-            #Code for an automatic login option
-            #Communicator.check_dir(Communicator.LOCAL_CONFIG_DIR_LINUX)
-                 
-
-            #Code for dynamically applying announcements
-            #announcement = SSH.get_log(Communicator.SSH_CONNECTION, Config.c['Logs'] + 'announcement.html')
-            #GUI.ANNOUNCEMENT_LABEL.setText(announcement)
-
-    #Code for a "Remember Login" option
-    #def check_dir(f):
-    #    d = os.path.dirname(f)
-    #    if not os.path.exists(d):
-    #        os.makedirs(d)
-
-    def enable_widgets():
-        GUI.CHAT_ENTRY.setEnabled(True)
-        GUI.SUBMIT_BTN.setEnabled(True)
-        GUI.CHANNEL_NOTEBOOK.setEnabled(True)
-        GUI.CONTENT_NOTEBOOK.setTabEnabled(1, True)
-        GUI.USER_BTN.setEnabled(True)
-
-    def check_user_permissions(channel):
-        for i in range(0,len(GUI.CHANNELS)):
-            if channel == GUI.CHANNELS[i][GUI.CHANNEL_NAME]:
-                for j in range(len(Communicator.USER_PERMISSIONS)):
-                    for k in range(len(GUI.CHANNELS[i][GUI.PERMISSIONS])):
-                        if Communicator.USER_PERMISSIONS[j] == GUI.CHANNELS[i][GUI.PERMISSIONS][k]:
-                            return True
-        return False
-    
-    def invalid_permissions():
-        # If user does not have appropriate permissions, print out message.
-        GUI.CHANNEL_DISPLAY.setPlainText("You do not have sufficient privileges to view this channel. If you feel this is in error, please contact an administrator")
-
-
 
 
 def main():
