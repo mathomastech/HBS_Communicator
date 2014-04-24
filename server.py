31b956a3fa45fae4d97970da98cb3ecc2bfca231
Marc Thomas <mathomastech@gmail.com>
2014-04-24 14:45:29 -0400

    Updated server

--- server.py
+++ server.py
@@ -1,42 +1,33 @@
-'''
-This file is to be run on the remote server that hosts the logs
-It's primary purpose is to reduce the network requirements of the
-client applications and shift the processing to the remote server
+import os, sys, os.path, time, socketserver
+from online import Online
 
-This will produce less network bandwidth and better performance 
-overall
-'''
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        time.sleep(.2)
+        self.data = str(self.request.recv(1024),"utf-8")
+        self.argv = self.data.rstrip().split(',')
+        self.time_stamps = ""
+        Online.add(self.argv[0])
+        Online.remove()
+        for i in range(1,len(self.argv)):
+            self.temp = ""
+            if not os.path.exists(self.argv[i]):
+                f = open(self.argv[i], 'w')
+                f.write("Fresh Log\n\n")
+                f.close()
+            self.temp = os.stat(self.argv[i])
+            self.time_stamps += str(self.temp.st_mtime) + ", "
 
-import sys, os.path, time
+        self.request.sendall(bytes(self.time_stamps + "\n", "utf-8"))
+    
+if __name__ == "__main__":
+    HOST, PORT = "0.0.0.0", 9998
 
-#argv[0] = Channel Name, argv[1] = Log Path, argv[2] = Client Log, argv[3] = User Path
 
-def main(argv):
-    flag = True
-    count = 0
-    if not os.path.exists(argv[3]):
-        os.makedirs(argv[3])
-    if not os.path.exists(argv[2]):
-        f = write("Fresh Log")
-        f.close()
-    while flag:
-        try:
-            time.sleep(1)
-            server = open(argv[1], 'r')
-            client = open(argv[2], 'r')
-            server_log = server.read()
-            client_log = client.read()
-            if client_log != server_log:
-                client = open(argv[2], 'w')
-                client.write(server_log)
-                print(argv[0])
-                flag = False
-            else:
-                count = count+1
-        except FileNotFoundError:
-                # Irrelevant line of code. When a file is being written to, it throws
-                # File not found error, causing the loop to exit. This gives it something
-                # to do as the file is being writted 
-                one=1
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
-main(sys.argv[1:])
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
