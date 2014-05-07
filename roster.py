e95d91188876928023ace6c838ad6e7eac00f636
Marc Thomas <mathomastech@gmail.com>
2014-05-07 11:26:51 -0400

    Sort order is now dynamic

--- roster.py
+++ roster.py
@@ -4,19 +4,8 @@ import mysql.connector
 class Roster:
     DB = ""
     ROSTER = []
-    SORT_ORDER = ['Commander in Chief','Deputy Commander in Chief','General of the Army','Operations Officer', 'Logistics Officer',
-            'Technical Expert Command Officer','SocO Officer','Deputy OpsO', 'Deputy LogO','CIA','Regulations Officer','General Administrative Officer',
-            'Brigade Officer','MP Commanding Officer', 'MP Executive Officer', 'Senior MP','Director of Web Services','Director of Financial Affairs', 'Chief of Staff','Director of Entertainment', 'Director of Admissions','Director of Social Media',
-            'Awards Director', 'Editor In Chief','Senior Admissions Officer',
-            'Titanfall Commanding Officer','Titanfall Executive Officer','Titanfall Chief Training Officer','CoD Commanding Officer',
-            'CoD Executive Officer','CoD Chief Training Officer','DayZ Commanding Officer','DayZ Executive Officer','DayZ Chief Training Officer',
-            'GW2 Commanding Officer','GW2 Executive Officer','GW2 Chief Training Officer','WoW Commanding Officer','WoW Executive Officer',
-            'WoW Chief Training Officer','LoL Platoon Leader','Minecraft Platoon Leader','CS:GO Platoon Leader',
-            '5* General','General','Lieutenant General','Major General','Brigadier General','Colonel', 'Lieutenant Colonel',
-            'Major','Captain','First Lieutenant','Second Lieutenant','Command Sergeant Major', 'Sergeant Major',
-            'First Sergeant','Master Sergeant','Sergeant First Class','Staff Sergeant','Sergeant','Corporal',
-            'Private First Class','Private','Recruit']
-
+    SORT_ORDER = []
+    
     def establish_connection():
         Roster.DB = mysql.connector.connect(host=Config.c['DB IP'],
                             user=Config.c['DB User'],
@@ -24,6 +13,11 @@ class Roster:
                             db=Config.c['DB Name']
                             )
 
+    def get_roster():
+        Roster.get_roster_raw()
+        Roster.sort_roster()
+        return Roster.ROSTER
+    
     def get_roster_raw():
         Roster.establish_connection()
         Roster.ROSTER = []
@@ -37,6 +31,13 @@ class Roster:
         for(gname, rank, username) in cur:
             Roster.ROSTER.append([gname,username,rank])
 
+        query = ("select rank \
+                 from gbgraphi_vbulletin.clan_members_ranks \
+                order by rankorder;")
+        cur.execute(query)
+        for rank in cur:
+            Roster.SORT_ORDER.append(rank[0])
+        
     def sort_roster():
         temp_roster = []
         for i in range (0,len(Roster.SORT_ORDER)):
@@ -47,7 +48,3 @@ class Roster:
         Roster.ROSTER = temp_roster
 
 
-    def get_roster():
-        Roster.get_roster_raw()
-        Roster.sort_roster()
-        return Roster.ROSTER
