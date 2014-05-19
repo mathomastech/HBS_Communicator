d0ed38ba10c799c907d2b9e8823bf0410e4511ea
Marc Thomas <mathomastech@gmail.com>
2014-05-19 11:00:17 -0500

    Rearranging code, removed old code

--- handler.py
+++ handler.py
@@ -59,6 +59,37 @@ class Handler(QtGui.QMainWindow):
         self.setWindowIcon(self.app_icon)
         self.show()
    
+    def channel_clicked(self,channel,flag):
+        Communicator.switch_channel(channel)
+    
+    def connect_elements(self):
+        GUI.CHANNEL_DISPLAY = self.com.channelDisplay
+        GUI.CHAT_ENTRY = self.com.chatEntry
+        GUI.SUBMIT_BTN = self.com.submitButton
+        GUI.ROSTER_DISPLAY = self.com.rosterDisplay
+        GUI.LOGIN_STATUS_LABEL = self.com.loginStatusLabel
+        GUI.USER_BTN = self.com.userButton
+        GUI.REFRESH_BUTTON = self.com.refreshButton        
+        GUI.CHANNEL_NOTEBOOK = self.com.channelNotebook
+        GUI.CONTENT_NOTEBOOK = self.com.contentNotebook
+        GUI.ANNOUNCEMENT_LABEL = self.com.announcementLabel
+        GUI.LOGIN_GROUP_BOX = self.com.loginGroupBox
+        GUI.USERNAME_ENTRY = self.com.usernameEntry 
+        GUI.PASSWORD_ENTRY = self.com.passwordEntry
+        GUI.REMEMBER_LOGIN_CHECK = self.com.rememberLoginCheck
+        GUI.CHANNEL_TAB = self.com.channelNotebook
+        # Disabled the Roster tab. 
+        # Move this line into ui_communicator.py either manually or
+        # through QTDesigner somehow. Qt Designer does not appear
+        # to allow me to disable individual tabs in a notebook,
+        # only the notebook as a while.
+        GUI.USERNAME_ENTRY.setFocus()
+        GUI.CONTENT_NOTEBOOK.setTabEnabled(1, False)
+        GUI.ONLINE_LAYOUT = self.com.onlineLayout
+
+    def on_chatEntry_returnPressed(self, *args):
+        self.on_submitButton_clicked(self, *args)
+    
     def generate_channels(self):
         for i in range(0,len(Communicator.CHANNEL_LIST)):
             GUI.generate_channel_info(Communicator.CHANNEL_LIST[i][0],
@@ -84,35 +115,23 @@ class Handler(QtGui.QMainWindow):
             elif GUI.CHANNELS[i][GUI.CHANNEL_GROUP] == "general": 
                  self.com.generalLayout.addWidget(self.button)
 
-    def channel_clicked(self,channel,flag):
-        Communicator.switch_channel(channel)
-
     def on_loginButton_pressed(self, *args):
         username = self.USERNAME_ENTRY.text()
         password = self.PASSWORD_ENTRY.text()
         Communicator.login(username,password)
-
-    def on_submitButton_clicked(self, *args):
-        Communicator.write_chat_to_channel()
-
-    def on_chatEntry_returnPressed(self, *args):
-        self.on_submitButton_clicked(self, *args)
-
+    
     def on_passwordEntry_returnPressed(self, *args):
         self.on_loginButton_pressed(self, *args)
     
-    def on_usernameEntry_returnPressed(self, *args):
-        self.on_loginButton_pressed(self, *args)
-
-    #def on_usernameEntry_activate(self, *args):
-    #    Handler.on_loginButton_pressed(self, *args)
-
     def on_rememberLoginCheck_stateChanged(self, *args):
         self.flag = args
         if self.flag[0] == 0:
             Communicator.REMEMBER_LOGIN = False
         else:
             Communicator.REMEMBER_LOGIN = True
+    
+    def on_submitButton_clicked(self, *args):
+        Communicator.write_chat_to_channel()
     
     def online_list(self):
         # Get a list of all currently logged in users
@@ -129,29 +148,7 @@ class Handler(QtGui.QMainWindow):
             self.button.setAutoDefault(True)
             GUI.ONLINE_LAYOUT.addWidget(self.button)
 
+    def on_usernameEntry_returnPressed(self, *args):
+        self.on_loginButton_pressed(self, *args)
 
-    def connect_elements(self):
-        GUI.CHANNEL_DISPLAY = self.com.channelDisplay
-        GUI.CHAT_ENTRY = self.com.chatEntry
-        GUI.SUBMIT_BTN = self.com.submitButton
-        GUI.ROSTER_DISPLAY = self.com.rosterDisplay
-        GUI.LOGIN_STATUS_LABEL = self.com.loginStatusLabel
-        GUI.USER_BTN = self.com.userButton
-        GUI.REFRESH_BUTTON = self.com.refreshButton        
-        GUI.CHANNEL_NOTEBOOK = self.com.channelNotebook
-        GUI.CONTENT_NOTEBOOK = self.com.contentNotebook
-        GUI.ANNOUNCEMENT_LABEL = self.com.announcementLabel
-        GUI.LOGIN_GROUP_BOX = self.com.loginGroupBox
-        GUI.USERNAME_ENTRY = self.com.usernameEntry 
-        GUI.PASSWORD_ENTRY = self.com.passwordEntry
-        GUI.REMEMBER_LOGIN_CHECK = self.com.rememberLoginCheck
-        #GUI.ONLINE_LIST = self.com.onlineList
-        GUI.CHANNEL_TAB = self.com.channelNotebook
-        # Disabled the Roster tab. 
-        # Move this line into ui_communicator.py either manually or
-        # through QTDesigner somehow. Qt Designer does not appear
-        # to allow me to disable individual tabs in a notebook,
-        # only the notebook as a while.
-        GUI.USERNAME_ENTRY.setFocus()
-        GUI.CONTENT_NOTEBOOK.setTabEnabled(1, False)
-        GUI.ONLINE_LAYOUT = self.com.onlineLayout
+
